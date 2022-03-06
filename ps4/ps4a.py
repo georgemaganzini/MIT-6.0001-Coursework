6a33892bf72decc6b65674c87a567f02453f7d2f
george2 <involver7@gmail.com>
2022-03-06 14:52:25 -0800

    Start pset 4

--- ps4/ps4a.py
+++ ps4/ps4a.py
@@ -8,7 +8,7 @@ def get_permutations(sequence):
     Enumerate all permutations of a given string
 
     sequence (string): an arbitrary string to permute. Assume that it is a
-    non-empty string.  
+    non-empty string.
 
     You MUST use recursion for this part. Non-recursive solutions will not be
     accepted.
@@ -23,7 +23,16 @@ def get_permutations(sequence):
     a different order than what is listed here.
     '''
 
-    pass #delete this line and replace with your code here
+
+
+    if len(sequence) <= 1:
+        return [sequence]
+    else:
+        perms = []
+        for e in get_permutations(sequence[:-1]):
+            for i in range(len(e)+ 1):
+                perms.append(e[:-1] + e[i:])
+        return perms
 
 if __name__ == '__main__':
 #    #EXAMPLE
@@ -31,10 +40,12 @@ if __name__ == '__main__':
 #    print('Input:', example_input)
 #    print('Expected Output:', ['abc', 'acb', 'bac', 'bca', 'cab', 'cba'])
 #    print('Actual Output:', get_permutations(example_input))
-    
+
 #    # Put three example test cases here (for your sanity, limit your inputs
-#    to be three characters or fewer as you will have n! permutations for a 
+#    to be three characters or fewer as you will have n! permutations for a
 #    sequence of length n)
 
-    pass #delete this line and replace with your code here
-
+    example_input = 'abc'
+    print('Input:', example_input)
+    print('Expected Output:', ['abc', 'acb', 'bac', 'bca', 'cab', 'cba'])
+    print('Actual Output:', get_permutations(example_input))
